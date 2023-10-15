6bb72c9d8066a5ba6e998a89fed9b4b99c4d308f
Raaj1500s <mr.k.manikanta99@gmail.com>
2023-10-15 10:04:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Count from './components/Count';
+
+function App() {
+  return (
+    <div>
+      <Count></Count>
+    </div>
+  );
+}
+
+export default App;
+/*
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Raaj1500s/ReactJSTask-1.git
+git push -u origin main*/
